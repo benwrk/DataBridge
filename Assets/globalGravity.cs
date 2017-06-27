7409e9d933caeec637df386d4e6dabe71dbd9d26
Sharma Anant PAL17K <anantsharma@kamk.fi>
2017-06-27 10:00:48 +0300

    I miss Git for Desktop

---
+++ Assets/globalGravity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalGravity : MonoBehaviour
+{
+
+    public Vector3 fullGravity;
+    public Vector3 microGravity;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        fullGravity = new Vector3(0, -1.5F, 0);
+        microGravity = new Vector3(0, -0.2f, 0);
+
+        Physics.gravity = fullGravity;
+
+
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            if (Physics.gravity.Equals(fullGravity))
+            {
+                Physics.gravity = microGravity;
+                randomforce.speed = 0.7F;
+                
+            }
+            else
+            {
+                Physics.gravity = fullGravity;
+                randomforce.speed =0;
+            }
+        }
+
+
+       
+
+
+
+    }
+}
