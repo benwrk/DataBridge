89f3006e4af5837e99d405e1cdd1a91510313cde
Sharma Anant PAL17K <anantsharma@kamk.fi>
2017-06-27 16:54:38 +0300

    Changed the gravitysystem (2nd update) Room 2 created

---
+++ Assets/floating.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floating : MonoBehaviour
+{
+
+    public float ZeroGravityFloatStrenght= 8.22f;
+    public float ZeroGravityRandomRotationStrenght = 0.2f;
+    //bool gravityFlag = true;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * ZeroGravityFloatStrenght);
+            transform.Rotate(ZeroGravityRandomRotationStrenght, ZeroGravityRandomRotationStrenght, ZeroGravityRandomRotationStrenght);
+        }
+        
+    }
+
+}
