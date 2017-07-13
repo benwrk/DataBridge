d6c24c87f378451f51c422fdb23b14618907f41c
Anant Sharma <ananttheant4444@gmail.com>
2017-07-13 14:10:56 +0300

    Bot Animation has Started and bot manager is made, with Function "ToggleFreezeOfPlayer(bool)"

---
+++ Assets/Scripts/BotCutSceneMovement/BotScript/BotAnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class BotAnimationManager : MonoBehaviour {
+
+    RigidbodyFirstPersonController controller;
+    bool Freeze;
+
+    Animator BotAnimator;
+
+    // Use this for initialization
+    void Start () {
+
+        BotAnimator = GetComponent<GameObject>().GetComponent<Animator>();
+        Freeze = BotAnimator.GetBool("frozen");
+        
+		
+	}
+
+
+
+    void ToggleFreezeOfPlayer(bool freeze)
+    {
+
+
+        controller.GetComponent<PlayerController>().isFrozen = !freeze;
+    }
+
+
+
+
+}
