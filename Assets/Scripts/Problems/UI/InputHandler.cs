dfd887ac390521f7c52687f9b284c34646fc997e
Anant Sharma <ananttheant4444@gmail.com>
2017-07-25 15:20:58 +0300

    UI for Input Questions done. ( use the function of InputHandler for getting the variable)

---
+++ Assets/Scripts/Problems/UI/InputHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class InputHandler : MonoBehaviour {
+
+    string currentString;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void EndEditListener(string s)
+    {
+        currentString = s;
+    }
+
+    public void PassString(string neededString)
+    {
+        Debug.Log(currentString);
+        currentString = null;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+
+
+
+}
