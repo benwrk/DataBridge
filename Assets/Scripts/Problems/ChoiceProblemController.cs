2a7e143f06d307e8c99ae1e8adcfb46479bfa593
Benjapol Worakan <benwrk@live.com>
2017-07-28 13:26:24 +0300

    ChoiceProblemController internal working modified and documented

--- Assets/Scripts/Problems/ChoiceProblemController.cs
+++ Assets/Scripts/Problems/ChoiceProblemController.cs
@@ -2,52 +2,46 @@
 using Data.Models.Problems;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityStandardAssets.Characters.FirstPerson;
 
 namespace Problems
 {
     public class ChoiceProblemController : MonoBehaviour
     {
-        public GameManager GameManager;
-        public GameObject Controller;
-        public RigidbodyFirstPersonController RbController;
-
-        public Text QuestionText;
-        public Text ProblemText;
-    
-        public int ProblemNumber;
-        public List<Text> ChoiceTexts;
-
         private IList<ChoiceQuestion> _randomizedChoiceQuestions;
-        
+        public List<Text> ChoiceTexts;
+        public GameManager GameManager;
+        public int ProblemNumber;
+        public Text ProblemText;
+        public Text QuestionText;
+
         private void Start()
         {
             var problem = (ChoiceProblem) GameManager.Problems[ProblemNumber - 1];
             ProblemText.text = problem.Text;
 
             _randomizedChoiceQuestions = GameUtility.CloneListWithRandomOrder(problem.Questions);
-            LoadQuestion(_randomizedChoiceQuestions, 0);
+            LoadQuestionAndTagCorrectChoice(_randomizedChoiceQuestions[0]);
         }
 
-        private void LoadQuestion(IList<ChoiceQuestion> questions, int questionNumber)
+        /// <summary>
+        /// Load the given question onto the UI, and also tag the correct choice.
+        /// </summary>
+        /// <param name="question">The question to be loaded.</param>
+        private void LoadQuestionAndTagCorrectChoice(ChoiceQuestion question)
         {
-            var question = questions[questionNumber];
             QuestionText.text = question.Text;
-
             var choices = GameUtility.CloneListWithRandomOrder(question.Choices);
 
             for (var i = 0; i < ChoiceTexts.Count && i < choices.Count; i++)
             {
                 ChoiceTexts[i].text = choices[i].Text;
                 if (choices[i].IsCorrect)
-                {
                     ChoiceTexts[i].tag = Constants.CorrectChoiceTag;
-                }
             }
         }
 
         /// <summary>
-        /// For Fungus to check if the selected choice is correct.
+        ///     For Fungus to check if the selected choice is correct.
         /// </summary>
         /// <param name="selectedChoiceText">The text component of the selected choice</param>
         /// <returns>True if the selected choice is the correct choice, false otherwise</returns>
@@ -55,37 +49,5 @@ namespace Problems
         {
             return selectedChoiceText.CompareTag(Constants.CorrectChoiceTag);
         }
-
-        //// Use this for initialization
-        //private void Start()
-        //{
-        //    Cursor.visible = false;
-
-        //}
-
-        //private void ToggleFreezeOfPlayer()
-        //{
-        //    Controller.GetComponent<PlayerController>().ToggleFreeze();
-        //    RbController.lookRotationEnabled = !RbController.lookRotationEnabled;
-        //    if (Cursor.lockState == CursorLockMode.Locked)
-        //    {
-        //        Cursor.lockState = CursorLockMode.Confined;
-        //        Debug.Log(Cursor.lockState);
-        //    }
-        //    else if (Cursor.lockState == CursorLockMode.Confined)
-        //    {
-        //        Cursor.lockState = CursorLockMode.Locked;
-        //        Debug.Log(Cursor.lockState);
-        //    }
-
-        //    if (Cursor.visible)
-        //    {
-        //        Cursor.visible = false;
-        //    }
-        //    else if (!Cursor.visible)
-        //    {
-        //        Cursor.visible = true;
-        //    }
-        //}
     }
 }
