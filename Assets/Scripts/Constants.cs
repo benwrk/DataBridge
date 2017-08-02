a258087c8848213dd21b7fa72f1a05413e279433
Benjapol Worakan <benwrk@live.com>
2017-08-02 13:04:30 +0300

    Update Constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,15 +1,15 @@
 ﻿public static class Constants
 {
-    public const float ZeroGravityFloatStrength = 10f;
-    public const float ZeroGravityRandomRotationStrength = 0.2f;
-    public const float ZeroGravityRotationSpeed = 100f;
     public const float DefaultForwardSpeed = 5.0f;
     public const float DefaultBackwardSpeed = 3.0f;
     public const float DefaultStrafeSpeed = 3.0f;
     public const float DefaultJumpForce = 40f;
     public const float GrabbingCameraCenterRange = 3f;
-    public const string GrabbableTag = "Pickable";
+    public const float ZeroGravityFloatStrength = 10f;
+    public const float ZeroGravityRandomRotationStrength = 0.2f;
+    public const float ZeroGravityRotationSpeed = 100f;
     public const string CorrectChoiceTag = "Correct";
+    public const string GrabbableTag = "Pickable";
     public const string UntaggedTag = "Untagged";
 
     public static class XmlParser
@@ -26,21 +26,19 @@
         public static class Problems
         {
             public const string ConfigFilePath = "Assets/XmlConfigs/problems.xml";
-            public const string LevelTagName = "Level";
-            public const string ChoiceQuestionTagName = "ChoiceQuestion";
-            public const string InputQuestionTagName = "InputQuestion";
-            public const string TextAttributeName = "text";
+            public const string ContainsRuleTagName = "ContainsRule";
             public const string ChoiceProblemTagName = "ChoiceProblem";
             public const string InputProblemTagName = "InputProblem";
             public const string IsCorrectAttributeName = "correct";
-            public const string PlaceholderAttributeName = "placeholder";
+            public const string LevelTagName = "Level";
             public const string MatchesRuleTagName = "MatchesRule";
-            public const string ContainsRuleTagName = "ContainsRule";
-            public const string WithoutRuleTagName = "WithoutRule";
             public const string PhraseAttributeName = "phrase";
+            public const string PlaceholderAttributeName = "placeholder";
             public const string RuleValidationOptionAttributeName = "rule-validation";
+            public const string TextAttributeName = "text";
             public const string ValidateAllAttributeValue = "all";
             public const string ValidateAnyAttributeValue = "any";
+            public const string WithoutRuleTagName = "WithoutRule";
         }
     }
 }
