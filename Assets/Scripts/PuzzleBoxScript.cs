dcef7e685f8e95633759326802cb1a9325fd78b1
Anant Sharma <ananttheant4444@gmail.com>
2017-08-15 12:58:12 +0300

    fixed the Puzzle boxes
    Loviisa, break them again, I dare you.

---
+++ Assets/Scripts/PuzzleBoxScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PuzzleBoxScript : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        if (!GameStates.FloatingObjectsEnabled)
+            MassIncrease();
+        else
+        {
+            GetComponent<Rigidbody>().mass = 1;
+        }
+    }
+
+    private void MassIncrease()
+    {
+        GetComponent<Rigidbody>().mass = 10;
+    }
+}
